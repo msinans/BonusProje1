3f565797fa151ae1f8ee158d3b56263172fe1835
m.sinans <msomercikoglu@gmail.com>
2023-09-03 22:59:53 +0300

    SqlConnection ekleyeceğim

---
+++ BonusProje1/FrmOgrenciNotlar.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace BonusProje1
+{
+    public partial class FrmOgrenciNotlar : Form
+    {
+        public FrmOgrenciNotlar()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglanti = new SqlConnection(@""); // Buradaki @ işareti bunun bir adres olduğunu programımıza bildirir.
+        private void FrmOgrenciNotlar_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
